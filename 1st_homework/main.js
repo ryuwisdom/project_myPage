eb4edde049be69c7481fdb7103a79aa592ada946
jihyeryu <bonnie.jr54@gmail.com>
2020-09-17 11:42:29 +0900

    index page add transition

--- 1st_homework/main.js
+++ 1st_homework/main.js
@@ -1,3 +1,10 @@
 const bottomSlide = document.getElementById('bottom');
-bottomSlide.classList.remove('bottom-invisiable'); 
-bottomSlide.classList.add('bottom-visiable');
+
+// bottomSlide.classList.remove('bottom-invisiable');
+// bottomSlide.classList.add('bottom-visiable');
+
+
+setTimeout(function () {
+    bottomSlide.classList.remove('bottom-invisiable');
+    bottomSlide.classList.add('bottom-visiable');
+}, 600);
